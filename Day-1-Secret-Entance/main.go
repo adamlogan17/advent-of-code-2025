43b8fd2a2e80e8dd898b103546b626c2d87b8392
Adam Logan <adamlogan42@gmail.com>
2025-12-08 18:58:46 +0000

    Add part 2

--- Day-1-Secret-Entance/main.go
+++ Day-1-Secret-Entance/main.go
@@ -16,10 +16,8 @@ func ReadFile(filename string) ([]string) {
 	return arr
 }
 
-func main() {
-	input := ReadFile("input.txt")
-	// testInput := []string{"L68", "L30", "R48", "L5", "R60", "L55", "L1", "L99", "R14", "L82"}
-	var dial int = 50
+func Part1(input []string, startDial int) {
+	var dial int = startDial
 	var count int = 0
 
 	for _, instruction := range input {
@@ -56,4 +54,51 @@ func main() {
 	}
 
 	fmt.Println(count)
+}
+
+func Part2(input []string, startDial int) {
+	var dial int = startDial
+	var count int = 0
+
+	for _, instruction := range input {
+		var direction string = string(instruction[0])
+		fmt.Println(direction)
+		var raw_rotations string = string(instruction[1:])
+		rotations, err := strconv.Atoi(raw_rotations)
+		if err != nil {
+			fmt.Println("Error parsing rotations as int")
+		}
+		fmt.Println(rotations)
+
+        step := 1
+        if direction == "L" {
+            step = -1
+        }
+
+        // Simulate step-by-step movement
+        for i := 0; i < rotations; i++ {
+            dial += step
+            if dial < 0 {
+                dial = 99
+            } else if dial > 99 {
+                dial = 0
+            }
+
+            // Count every time the dial points at 0
+            if dial == 0 {
+                count++
+            }
+        }
+		fmt.Println(dial)
+		fmt.Println()
+    }
+	fmt.Println(count)
+}
+
+func main() {
+	input := ReadFile("input.txt")
+	// input := []string{"L68", "L30", "R48", "L5", "R60", "L55", "L1", "L99", "R14", "L82"}
+	start := 50
+	Part1(input, start)
+	Part2(input, start)
 }
