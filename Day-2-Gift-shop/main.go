844b81e2404b8605e844fd70c9808a13d5a8b432
Adam Logan <adamlogan42@gmail.com>
2025-12-30 17:06:01 +0000

    Day 2 solution

--- Day-2-Gift-shop/main.go
+++ Day-2-Gift-shop/main.go
@@ -18,7 +18,32 @@ func ReadFile(filename string) ([]string) {
 	return arr
 }
 
-func pattern_check(input string) (bool) {
+func Part2PatternCheck(input string) (bool) {
+	inputLen := len(input)
+	for i := 1; i < inputLen; i++ {
+		match := true
+		sequence := string(input[:i])
+		sequenceLen := len(sequence)
+		if inputLen % len(sequence) != 0 {
+			continue
+		}
+		for j := sequenceLen; j < inputLen; j+=sequenceLen {
+			finalSequenceIndex := j+sequenceLen
+			checkSequence := string(input[j:finalSequenceIndex])
+			if checkSequence != sequence {
+				match = false
+				break
+			}
+		}
+		if match == true {
+			// fmt.Println("match seq", sequence)
+			return true
+		}
+	}
+	return false
+}
+
+func Part1PatternCheck(input string) (bool) {
 	inputLen := len(input)
 	if inputLen % 2 != 0 {
 		return false
@@ -37,9 +62,7 @@ func pattern_check(input string) (bool) {
 func main() {
 	input := ReadFile("input.txt")
 	// input := []string{"11-22", "95-115", "998-1012" , "1188511880-1188511890", "222220-222224", "1698522-1698528", "446443-446449", "38593856-38593862", "565653-565659", "824824821-824824827", "2121212118-2121212124"}
-	// fmt.Println(input)
 	total := 0
-
 	for _, id := range input {
 		id_range := strings.Split(id, "-")
 		start, startErr := strconv.Atoi(id_range[0])
@@ -50,8 +73,10 @@ func main() {
 
 		for i := start; i <= end; i++ {
 			iAsStr := strconv.Itoa(i)
-			isPattern := pattern_check(iAsStr)
+			isPattern := Part2PatternCheck(iAsStr)
 			if isPattern {
+				// fmt.Println("Invalid id found", i)
+				// fmt.Println("=========")
 				total += i
 			}
 		}
